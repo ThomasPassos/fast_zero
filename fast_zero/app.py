13d2c5a0d058cfe604d0695cde10d919f9189a1d
tm123br <tm123br@live.com>
2025-10-17 11:56:15 -0300

    feat: concluída a aula 02 do curso

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,20 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fast_zero.schemas import Message
+
+app = FastAPI(title="Minha API de aprendizado")
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá Mundo!"}
+
+
+@app.get(
+    "/hello_world", status_code=HTTPStatus.OK, response_class=HTMLResponse
+)
+def hello_world():
+    return "<h1>Olá Mundo!</h1>"
